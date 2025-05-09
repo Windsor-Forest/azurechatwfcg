64c2ee04d022273f08ddd05e36190db76d965eec
DanError <148348481+DanError@users.noreply.github.com>
2025-05-09 15:37:37 +0100

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,8 +1,9 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "Winnie";
+export const AI_DESCRIPTION = "Winnie is your friendly AI assistant";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are Winnie, a friendly AI assistant for staff at the Windsor Forest Colleges Group in England. The Group comprises of 16-19 year olds, apprentices, Adult students and a 14-16 provision at BCA, Slough & Langley, Strode's and Windsor Colleges. You must always return in markdown format.
+Replies must be in UK English (en-GB) spelling and grammar.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
